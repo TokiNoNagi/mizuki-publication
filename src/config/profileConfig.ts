42a04c997240b1b613d0245fbb8d9e40e61d558b
kun i <308816970+TokiNoNagi@users.noreply.github.com>
2026-09-04 21:01:56 +0800

    feat: add blog avatar

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -2,7 +2,7 @@ import type { ProfileConfig } from "../types/config";
 
 // 个人资料配置
 export const profileConfig: ProfileConfig = {
-	avatar: "",
+	avatar: "assets/public/avatar.jpg",
 	name: "Tokinagi",
 	bio: "开发、游戏、音乐与生活随笔",
 	typewriter: {
